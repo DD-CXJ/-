4aed285038500e526f0738c8f9c14ca1ed36c2ff
陈兴基 <63285294+DD-CXJ@users.noreply.github.com>
2020-04-25 20:16:52 +0800

    车票购票程序2.0
    可按人数购买车票

---
+++ Main.java
@@ -0,0 +1,17 @@
+package 车票购票程序;
+
+/**
+ * @author 迪迪
+ * @date 2020/4/25
+ **/
+public class Main {
+    private int calculation;
+
+    public int getCalculation() {
+        return calculation;
+    }
+
+    public void setCalculation(int calculation) {
+        this.calculation = calculation*10;
+    }
+}
--- Ticket.java
+++ Ticket.java
@@ -9,30 +9,32 @@ import java.util.Scanner;
  **/
 public class Ticket {
     public static void main(String[] args) {
-        /**售票机1.0
+        /**售票机2.0
          * 1·打印车票（输出）
          * 2·输入金额信息（输入）
          * 3·条件判断if
          * 4·循环while for
          * */
-       int money,price,amount;
+        Main c=new Main();
+       int money,amount;
         Scanner in=new Scanner(System.in);
+        System.out.println("一张车票价格是10元");
         System.out.println("请输入您需要购买几张车票");
-        System.out.println("请投币");
+        c.setCalculation(in.nextInt());
+        System.out.println("您的车票价格"+c.getCalculation()+"元请投币");
         money=in.nextInt();//接收键盘输入的值
-        price=10;//车票价格10元
         amount=money;//把键盘输入的值赋值给amount
         //判断amout是否小于车票10元
-        while (amount<price){
+        while (amount<c.getCalculation()){
             System.out.println("投币不足,请继续投币");
             money=in.nextInt();
             amount=amount+money;
         }
         System.out.println("****************");
         System.out.println("**  迪迪铁路   **");
-        System.out.println("*车票   "  +price+"    元*");
+        System.out.println("*车票   "+c.getCalculation()+"   元*");
         System.out.println("****************");
-        int change=amount-price;
+        int change=amount-c.getCalculation();
         System.out.println("找您"+change+"元");
     }
 }
